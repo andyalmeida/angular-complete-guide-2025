7413466fb21bc6575b4a1aad33a5ca4d3b4eba08
André Almeida <andalvalmeida@gmail.com>
2025-06-14 14:21:30 -0300

    refactor: move Task and User interfaces to separate model files for better organization

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
