9cd0417967c4a2d2f2c436086105e68aa25cf5f4
André Almeida <andalvalmeida@gmail.com>
2025-06-14 12:12:33 -0300

    refactor: user component to accept user object and simplify template

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,21 +1,25 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'users/' + this.avatar;
+    return 'users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
