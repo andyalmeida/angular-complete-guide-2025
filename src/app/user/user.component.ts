bc9d65214a66e3a6cba3fc4ab0b13368dd189823
André Almeida <andalvalmeida@gmail.com>
2025-06-14 11:35:34 +0000

    feat: add dummy users and update user component to display random user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent {}
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return 'users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    const newIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[newIndex];
+  }
+}
